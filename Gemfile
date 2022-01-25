970f05404cd3e81cf6029a653a0c22cb80f7c54f
Robespierry <gleb.kovalenko66@gmail.com>
2022-01-25 23:10:45 +0300

    Добавил webdriver manager, удалил файлы драйверов, добавил глобально Edisoft_Api_Client

--- Gemfile
+++ Gemfile
@@ -8,4 +8,5 @@ gem 'cucumber', '2.4.0'
 gem 'psych'
 gem 'rest-client'
 gem 'rspec', '~> 3.5'
-gem 'selenium-webdriver', '2.53.4'
+gem 'selenium-webdriver', '4.1.0'
+gem 'webdrivers', '~> 4.0', require: false
--- features/support/env.rb
+++ features/support/env.rb
@@ -3,31 +3,36 @@
 require 'rest-client'
 require 'active_support/all'
 require_relative 'helpers/rest_wrapper'
+require_relative 'helpers/file_system_helpers'
 require_relative 'helpers/logger'
+require_relative 'api_clients/edisoft_api_client'
 require 'capybara/cucumber'
 require 'selenium-webdriver'
 require_relative 'helpers/class_extentions'
+require 'webdrivers'
+
+$working_directory = Dir.pwd.gsub('/', '\\')
 
 def browser_setup(browser = 'firefox')
   case browser
   when 'chrome'
     Capybara.register_driver :chrome do |app|
-      Selenium::WebDriver::Chrome.driver_path = 'configuration/chromedriver'
       profile = Selenium::WebDriver::Chrome::Profile.new
       profile['profile.default_content_settings.popups'] = 0 # custom location
       profile['browser.helperApps.neverAsk.saveToDisk'] = 'application/octet-stream, text/xml'
       profile['pdfjs.disabled'] = true
       Capybara::Selenium::Driver.new(app, browser: :chrome,
-                                          desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
-                                            'chromeOptions' => {
-                                              'args' => ['--window-size=1920,1080'],
-                                              'prefs' => {
-                                                'download.default_directory' => Dir.pwd + '/features/tmp/',
-                                                'download.prompt_for_download' => false,
-                                                'plugins.plugins_disabled' => ['Chrome PDF Viewer']
-                                              }
-                                            }
-                                          ))
+                                     desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+                                       'goog:chromeOptions' => {
+                                         'args' => ['--window-size=1920,1080'],
+                                         'prefs' => {
+                                           'download.default_directory' => "#{$working_directory}\\features\\tmp",
+                                           'download.prompt_for_download' => false,
+                                           'download.directory_upgrade ' => true,
+                                           'plugins.plugins_disabled' => ['Chrome PDF Viewer']
+                                         }
+                                       }
+                                     ))
     end
     Capybara.default_driver = :chrome
     Capybara.page.driver.browser.manage.window.maximize
@@ -36,19 +41,23 @@ def browser_setup(browser = 'firefox')
   else
     Capybara.register_driver :firefox_driver do |app|
       profile = Selenium::WebDriver::Firefox::Profile.new
-      Selenium::WebDriver::Firefox.driver_path = 'configuration/geckodriver'
       profile['browser.download.folderList'] = 2 # custom location
-      profile['browser.download.dir'] = Dir.pwd + '/features/tmp/'
+      profile['browser.download.dir'] = "#{Dir.pwd}/features/tmp/"
       profile['browser.helperApps.neverAsk.saveToDisk'] = 'application/octet-stream, text/xml'
       profile['pdfjs.disabled'] = true
-      Capybara::Selenium::Driver.new(app, browser: :firefox, profile: profile, port: Random.rand(7000..7999))
+      profile['unhandledPromptBehavior'] = true
+
+      options = Selenium::WebDriver::Firefox::Options.new(profile: profile)
+      Capybara::Selenium::Driver.new(app, browser: :firefox, options: options, port: Random.rand(7000..7999))
     end
     Capybara.default_driver = :firefox_driver
   end
 end
+
 browser_setup('chrome')
 
 configuration = YAML.load_file 'configuration/default.yml'
-$rest_wrap = RestWrapper.new url: 'https://testing4qa.ediweb.ru/api',
-                             **configuration[:credentials]
+
+$edisoft_api_client = EdisoftApiClient.new url: 'https://testing4qa.ediweb.ru/api',
+                                           **configuration[:credentials]
 logger_initialize
